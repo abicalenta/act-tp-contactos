6aab519f20da81106411e80755b16f743d22f7e2
abigail calenta <abicalenta@gmail.com>
2025-10-20 09:07:39 -0300

    20/10

--- src/app/layouts/logged-layout.ts
+++ src/app/layouts/logged-layout.ts
@@ -1,8 +1,7 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { AuthService } from '../services/auth-service';
-import Swal from 'sweetalert2'
-
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-logged-layout',
@@ -14,20 +13,40 @@ export class LoggedLayout {
 
   authService = inject(AuthService);
 
-  /** Abre modal de pregunta de cerrar sesión */
+
   openLogoutModal(){
+    {
+      this.menuOpen = false;
+    // tu lógica de logout o modal
+    }
     Swal.fire({
       title: "¿Desea cerrar sesión?",
       showDenyButton: true,
       showCancelButton: true,
       showConfirmButton: false,
       cancelButtonText: "Cancelar",
-      denyButtonText: 'Cerrar sesión'
+      denyButtonText: Cerrar sesión
     }).then((result) => {
       if (result.isDenied) { //Reviso que haya clickeado en el botón rojo.
         this.authService.logout();
       }
     });
   }
+///
+menuOpen = false;
+
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  goToNewContact() {
+    this.menuOpen = false;
+    this.router.navigate(['/new-contact']);
+  }
+
+
+
 
 }
